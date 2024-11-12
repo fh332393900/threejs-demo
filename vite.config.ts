2014cfd8aec37f9a52f2b66a07bbd6a644acb7ec
fenghang <hang.feng@nx-engine.com>
2024-11-12 15:13:13 +0800

    fix: 修改vite.config base

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'development' ? './' : '/threejs-demo/',
+  base: './',
   server: {
     port: 8000,
   },
